64da3cb4914a9f43c7f795fde471512f98fd82ed
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-21 22:48:13 +0800

    added : topnav components and some amends at home component file

--- client/src/resources/js/Pages/Home.jsx
+++ client/src/resources/js/Pages/Home.jsx
@@ -1,8 +1,25 @@
+import TopNav from "../Components/TopNav";
+import { useSelector } from "react-redux";
+
 const Home = () =>{
+    const {auth} = useSelector((state)=>({...state}));
+
     return(
-        <div className="container-fluid">
-            <h1>Home</h1>
+        <>
+        <TopNav/>
+        <div className="container-fluid pb-3">
+        <div className="d-grid gap-3" style={{ gridTemplateColumns: '1fr 2fr' }}>
+          <div className="bg-light border rounded-3">
+            <pre>{JSON.stringify(auth,null,4)}</pre>
+
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          </div>
+          <div className="bg-light border rounded-3">
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          </div>
         </div>
+        </div>
+        </>
     )
 }
 
