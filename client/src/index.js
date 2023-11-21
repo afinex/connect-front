8693f3d5f36a7e365e082e5c419e645873e843db
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-21 20:33:33 +0800

    changes : applied bootstrap to global

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
