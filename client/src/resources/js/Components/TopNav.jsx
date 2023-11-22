bc395eddc21e32c5aefe100fa84b431f842b8093
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-23 02:41:26 +0800

    changes : clicking home on topnav will redirect to /

--- client/src/resources/js/Components/TopNav.jsx
+++ client/src/resources/js/Components/TopNav.jsx
@@ -14,7 +14,7 @@ const TopNav = () => {
                     </a>
 
                     <ul className="nav nav-pills col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li class="nav-item"><Link to="/" class="nav-link active" aria-current="page">Home</Link></li>
+                        <li className="nav-item"><Link to="/" className="nav-link active" aria-current="page">Home</Link></li>
                         <li><a href="#" className="nav-link px-2 link-secondary">Overview</a></li>
                         <li><a href="#" className="nav-link px-2 link-dark">Inventory</a></li>
                         <li><a href="#" className="nav-link px-2 link-dark">Customers</a></li>
