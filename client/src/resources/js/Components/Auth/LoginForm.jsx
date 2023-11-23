0f631fb1ee9a8ee1533fc8902b7dacb768ef3e59
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-23 15:04:16 +0800

    added : login page and login form now post data to db

--- client/src/resources/js/Components/Auth/LoginForm.jsx
+++ client/src/resources/js/Components/Auth/LoginForm.jsx
@@ -1,17 +1,43 @@
-const LoginForm = () =>{
-    return(
-    <form>
-        <div class="form-group p-2 m-3">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group p-2 m-3">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <button type="submit" class="btn btn-primary m-3">Login</button>
-      </form>)
-}
+const LoginForm = ({
+  handleSubmit,
+  email,
+  setEmail,
+  password,
+  setPassword,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group p-2 m-3">
+        <label htmlFor="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+          onChange={(e)=> setEmail(e.target.value)}
+          value={email}
+        />
+        <small id="emailHelp" className="form-text text-muted">
+          We'll never share your email with anyone else.
+        </small>
+      </div>
+      <div className="form-group p-2 m-3">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+          placeholder="Password"
+          onChange={(e)=> setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary m-3">
+        Login
+      </button>
+    </form>
+  );
+};
 
 export default LoginForm;
--- client/src/resources/js/Pages/Auth/Login.jsx
+++ client/src/resources/js/Pages/Auth/Login.jsx
@@ -1,9 +1,28 @@
 import TopNav from "../../Components/TopNav";
-import { useSelector } from "react-redux";
 import LoginForm from "../../Components/Auth/LoginForm";
+import { useState } from "react";
+import axios from "axios";
+import {toast} from "react-toastify";
 
 const Login = () =>{
-    const {auth} = useSelector((state)=>({...state}));
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async(e) =>{
+      e.preventDefault();
+
+      try {
+        const res = await axios.post(`${process.env.REACT_APP_API_SERVER}/login`,{
+          email:email,
+          password:password,
+        })
+
+        toast.success(`Logged in .`)
+        console.log(res);
+      } catch (error) {
+        toast.error(error.response.data);
+      }
+    }
 
     return(
       <>
@@ -11,7 +30,13 @@ const Login = () =>{
       <div className="container-fluid pb-3">
       <div className="d-grid gap-3" style={{ gridTemplateColumns: '1fr 2fr' }}>
         <div className="bg-light border rounded-3">
-          <LoginForm/>
+          <LoginForm
+          handleSubmit={handleSubmit}
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          />
         </div>
         <div className="bg-light border rounded-3">
           <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
