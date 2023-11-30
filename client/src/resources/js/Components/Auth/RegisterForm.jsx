67445ade96dc6d8e56c7d3745de33846e19cb4f2
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-11-30 23:03:02 +0800

    changes : "class" to "className" and "for" to "htmlFor"

--- client/src/resources/js/Components/Auth/RegisterForm.jsx
+++ client/src/resources/js/Components/Auth/RegisterForm.jsx
@@ -11,50 +11,50 @@ const RegisterForm = ({
 }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <div class="form-group p-2 m-3">
-        <label for="exampleInputUsername">Username</label>
+      <div className="form-group p-2 m-3">
+        <label htmlFor="exampleInputUsername">Username</label>
         <input
           type="text"
-          class="form-control"
+          className="form-control"
           id="exampleInputUsername"
           aria-describedby="UsernameHelp"
           placeholder="Enter username"
           value={username}
           onChange={(e)=>setUsername(e.target.value)}
         />
-        <small id="textHelp" class="form-text text-muted">
+        <small id="textHelp" className="form-text text-muted">
           We'll never share your username with anyone else.
         </small>
       </div>
 
-      <div class="form-group p-2 m-3">
-        <label for="exampleInputEmail1">Email address</label>
+      <div className="form-group p-2 m-3">
+        <label htmlFor="exampleInputEmail1">Email address</label>
         <input
           type="email"
-          class="form-control"
+          className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
           placeholder="Enter email"
           value={email}
           onChange={(e)=>setEmail(e.target.value)}
         />
-        <small id="emailHelp" class="form-text text-muted">
+        <small id="emailHelp" className="form-text text-muted">
           We'll never share your email with anyone else.
         </small>
       </div>
 
-      <div class="form-group p-2 m-3">
-        <label for="exampleInputPassword1">Password</label>
+      <div className="form-group p-2 m-3">
+        <label htmlFor="exampleInputPassword1">Password</label>
         <input
           type="password"
-          class="form-control"
+          className="form-control"
           id="exampleInputPassword1"
           placeholder="Password"
           value={password}
           onChange={(e)=>setPassword(e.target.value)}
         />
       </div>
-      <button type="submit" class="btn btn-primary m-3">
+      <button type="submit" className="btn btn-primary m-3">
         Register
       </button>
     </form>
