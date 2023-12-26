941066d854bace9a118560a0d6aff8e8d6182f52
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2023-12-26 15:32:02 +0800

    changes : check if redux state got data or not then optional render the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,18 +9,22 @@ import { useSelector } from "react-redux";
 import Home from "./resources/js/Pages/Home";
 import Dashboard from "./resources/js/Pages/Auth/Dashboard";
 import Profile from "./resources/js/Pages/Auth/Profile";
-
+import Messages from "./resources/js/Pages/Auth/Messages";
+import GeneralSetting from "./resources/js/Pages/Auth/User/Setting/General/GeneralSetting";
+import PasswordSetting from "./resources/js/Pages/Auth/User/Setting/Password/PasswordSetting";
 
 const App = () => {
-  const {data} = useSelector((state)=>({...state}));
+  const {data} = useSelector((state)=>({...state})) || {};
 
+  const isDataAvailable = data && Object.keys(data).length > 0;
+  
   return (
     <BrowserRouter>
     <Switch>
       <Route
         path="/login"
         render={() =>
-          data ? <Redirect to="/" /> : <Login />
+          isDataAvailable ? <Redirect to="/" /> : <Login />
         }
       >
       </Route>
@@ -28,14 +32,37 @@ const App = () => {
       <Route
         path="/register"
         render={() =>
-          data ? <Redirect to="/" /> : <Register />
+          isDataAvailable ? <Redirect to="/" /> : <Register />
+        }
+      >
+      </Route>
+
+      <Route 
+        path="/messages"
+        render={()=>isDataAvailable ? <Messages data={data}/> : <Redirect to="/login"></Redirect>
+        }
+      >
+      </Route>
+
+      <Route 
+        path="/setting/general"
+        render={()=>isDataAvailable ? <GeneralSetting data={data}/> : <Redirect to="/login"></Redirect>
         }
       >
       </Route>
       
-      <Route exact path="/" render={()=>data ? <Dashboard data={data}/> : <Home/>}></Route>
-
-      <Route exact path="/:username" render={()=>data ? <Profile data={data}/> : <Redirect to="/login"></Redirect>}></Route>
+      <Route 
+        path="/setting/password"
+        render={()=>isDataAvailable ? <PasswordSetting data={data}/> : <Redirect to="/login"></Redirect>
+        }
+      >
+      </Route>
+      
+      <Route exact path="/" render={()=>isDataAvailable ? <Dashboard data={data}/> : <Redirect to="/login"></Redirect>}></Route>
+      
+      
+      {/* this is for profile */}
+      <Route exact path="/:username" render={()=>isDataAvailable ? <Profile data={data}/> : <Redirect to="/login"></Redirect>}></Route>
       
     </Switch>
     </BrowserRouter>
