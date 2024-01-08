1f399b85a962465e6fb624d72b94cf19f99a1cbf
Shafin Aqrin <shafin.aqrin@newtycoon.com>
2024-01-09 03:55:27 +0800

    changes : will now fetch real data in dashboard content

--- client/src/resources/js/Pages/Auth/DashboardContent.jsx
+++ client/src/resources/js/Pages/Auth/DashboardContent.jsx
@@ -1,27 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HeartOutlined, MessageOutlined, ShareAltOutlined, BookOutlined } from '@ant-design/icons';
+import axios from 'axios';  // Make sure to import axios
 import SideNav from '../../Components/SideNav';
 
-const ConnectFeedItem = ({ userProfileImage, username, image, caption, likes, comments, shares }) => {
+import { getAccessTokenCookies } from '../../Helpers/functions';
+import { handleApiError } from '../../Helpers/apiUtil';
+
+import { useDispatch } from 'react-redux';
+
+import {InfoCircleTwoTone} from "@ant-design/icons";
+const ConnectFeedItem = (feedData) => {
   return (
     <div className="card mt-4">
       <div className="card-header d-flex align-items-center">
-        <img src={userProfileImage} className="rounded-circle mr-2" alt="User Profile" width="30" height="30" />
-        <strong>{username}</strong>
+        {/* <img src={userProfileImage} className="rounded-circle mr-2" alt="User Profile" width="30" height="30" /> */}
+        <strong>{feedData.user.username}</strong>
       </div>
-      <img src={image} className="card-img-top" alt="Instagram Post" />
+      <img src={`${import.meta.env.VITE_APP_PUBLIC_SERVER}/images/posts/${feedData.image.filename}`} className="card-img-top" alt="Instagram Post" />
       <div className="card-body">
         <div className="d-flex justify-content-between">
           <div>
             <button type="button" className="btn btn-link">
-              <HeartOutlined /> {likes}
+              <HeartOutlined /> 0
             </button>
             <button type="button" className="btn btn-link">
-              <MessageOutlined /> {comments}
+              <MessageOutlined /> 0
             </button>
             <button type="button" className="btn btn-link">
-              <ShareAltOutlined /> {shares}
+              <ShareAltOutlined /> 0
             </button>
           </div>
           <button type="button" className="btn btn-link">
@@ -29,8 +36,8 @@ const ConnectFeedItem = ({ userProfileImage, username, image, caption, likes, co
           </button>
         </div>
         <p className="card-text mt-2">
-          <img src={userProfileImage} className="rounded-circle mr-2" alt="User Profile" width="20" height="20" />
-          <strong>{username}</strong>: {caption}
+          {/* <img src={userProfileImage} className="rounded-circle mr-2" alt="User Profile" width="20" height="20" /> */}
+          <strong>{feedData.user.username}</strong> : {feedData.caption}
         </p>
       </div>
     </div>
@@ -38,37 +45,55 @@ const ConnectFeedItem = ({ userProfileImage, username, image, caption, likes, co
 };
 
 const DashboardContent = () => {
-  const feedData = [
-    {
-      id: 1,
-      userProfileImage: '/image_pp_1.jpeg', // Assuming image_pp_1.jpg is in the public folder
-      username: 'user1',
-      image: '/image_1.jpg', // Assuming image_1.jpg is in the public folder
-      caption: 'Beautiful scenery! 🌄',
-      likes: 120,
-      comments: 8,
-      shares: 5,
-    },
-    {
-      id: 2,
-      userProfileImage: '/image_pp_2.jpeg', // Assuming image_pp_2.jpg is in the public folder
-      username: 'user2',
-      image: '/image_2.jpg', // Assuming image_2.jpg is in the public folder
-      caption: 'Exploring the city streets. 🏙️',
-      likes: 200,
-      comments: 15,
-      shares: 10,
-    },
-    // Add more items as needed
-  ];
+  const dispatch = useDispatch();
+  const [feedData, setFeedData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const fetchAccessCookies = await getAccessTokenCookies();
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_API_SERVER}/fetch`,
+        {
+          headers: {
+            Authorization: `Bearer ${fetchAccessCookies}`,
+          },
+        }
+      );
+
+      if(response.data.success){
+        setFeedData(response.data.post);
+      }
+    } catch (error) {
+      const handleResult = await handleApiError(error, dispatch);
+      if (handleResult.success) {
+        fetchData();
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="container-fluid dashboard-content-container">
       <div className="row">
         <div className="col-md-5 offset-md-3">
-          {feedData.map(item => (
-            <ConnectFeedItem key={item.id} {...item} />
-          ))}
+        {/* {feedData && Object.keys(feedData).length > 0 && feedData.map(item => (
+            <ConnectFeedItem key={item._id} {...item} />
+          ))} */}
+
+        {Object.keys(feedData).length == 0 && (
+        <div>
+          <InfoCircleTwoTone style={{ fontSize: "22px" }} twoToneColor="#2fafeb" />
+          <span>Follow more to keep updated</span>
+        </div>)}
+
+        {feedData.map((item, index) => (
+          <div key={index}>
+            <ConnectFeedItem key={item._id} {...item} />
+          </div>
+        ))}
         </div>
       </div>
     </div>
